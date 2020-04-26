edf014e29b1df9ef06ae592b8fdc01e2ff772b45
Rin <rb1q84@gmail.com>
2020-04-26 23:19:29 +0300

    Подготовка к PlatMaker
    Создание нужных префабов, проверка работы функции для создания обьекта на сцене

---
+++ Assets/Scripts/CreationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationScript : MonoBehaviour
+{
+    public GameObject[] objects;
+
+
+    void Start()
+    {
+        Instantiate(objects[0], new Vector2(0,0), Quaternion.identity);
+        Instantiate(objects[9], new Vector3(0, 0,-1), Quaternion.identity);
+        Instantiate(objects[3], new Vector2(0,-50), Quaternion.identity);
+
+
+    }
+}
